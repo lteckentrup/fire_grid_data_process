24f272f099f9500d16722aa3e9faf958e65536f1
JinyanYang <jyyoung@live.com>
2022-03-30 10:49:09 +1100

    code

--- r/read_FH_data.R
+++ r/read_FH_data.R
@@ -1,3 +1,4 @@
+# check spatial distribution of FH data####
 library(rgdal)
 # set the crs (GDA 94) that the data is in 
 sincrs <- "+proj=lcc +lat_1=-36 +lat_2=-38 +lat_0=-37 +lon_0=145 +x_0=2500000 +y_0=2500000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs "
@@ -12,7 +13,7 @@ coords.repro.tmp <- spTransform(s, lonlat)
 coords.repro.df <- as.data.frame(coords.repro.tmp)
 # range(coords.repro.df$coords.x2)
 
-# plot on a map###
+# plot on a map####
 library(maps)
 # plot maps
 # show map with Latitude 200 as center
@@ -22,3 +23,5 @@ maps::map('world', xlim = c(130, 155),ylim=c(-45,-25))
 maps::map.axes()
 points(x = coords.repro.df$coords.x1,y=coords.repro.df$coords.x2,pch=16,cex=0.5,col='red')
 dev.off()
+
+# now check how to get a score#####
