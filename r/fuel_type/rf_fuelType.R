32a8da95bf34259ccb83c592be92ec06e66b0933
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-11-28 10:08:16 +1100

    Update rf_fuelType.R
    Attributes couldn't be read in from tif. Updated following Jim's suggestion

--- r/fuel_type/rf_fuelType.R
+++ r/fuel_type/rf_fuelType.R
@@ -22,14 +22,12 @@ source('r/function_rf.R')
 #####
 ft.in.df <- readRDS('cache/ft.met.lai.rds')
 ######remove non natual types##########
-evc.df <- raster('data/EVC_fuelType/evc/VICSANSW161.tif')
+atrribute.df <- foreign::read.dbf('data/EVC_fuelType/evc/VICSANSW161.tif.vat.dbf')
 
-atrribute.df <- evc.df@data@attributes[[1]]
-atrribute.df <- atrribute.df[,c('VICSANSW.FUEL_TYPE' , 'VICSANSW.TYPE_NAME')]
-nm.vec <- atrribute.df$VICSANSW.TYPE_NAME[atrribute.df$VICSANSW.FUEL_TYPE %in%
-                                            unique(ft.in.df$ft)]
-ft.nm.df <- data.frame(nm = atrribute.df$VICSANSW.TYPE_NAME[atrribute.df$VICSANSW.FUEL_TYPE %in%
-                                                              unique(ft.in.df$ft)],
+nm.vec <- atrribute.df$TYPE_NAME[atrribute.df$FUEL_TYPE %in%
+                                   unique(ft.in.df$ft)]
+
+ft.nm.df <- data.frame(nm = nm.vec,
                        ID = unique(ft.in.df$ft))
 
 ID.natue <- ft.nm.df$ID[!ft.nm.df$nm %in% c('Eaten Out Grass','Orchard / Vineyard',
