50c3c32a59cbbde080e7459360bb1c29a5f85b31
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-12-16 17:22:43 +1100

    Update Plot_importance.R
    Results not consistent with Jim's

--- r/Plot_importance.R
+++ r/Plot_importance.R
@@ -1,92 +1,33 @@
-### Increase memory: run ulimit -s 32768 from command line
 library(randomForest)
 
-### Read in fitted RF for fueltype shortlist 
-rf.fit.ft <- readRDS('../cache/rf.fit.fuelType.new.short.rds')
+# Read the random forest model object
+rf.fit.ft <- readRDS('../cache/rf.fit.fuelType.new.rds')
 
-### Calculate variable importance of RF
-varImpPlot(rf.fit.ft)
+# Define a function to plot predictor importance of RF model
+plot.importance.func <- function(rf.fit.ft) {
 
-### Define new varImpPlot function (???) why
-varImpPlot <- function (x, sort = TRUE, n.var = min(30, nrow(x$importance)), 
-          type = NULL, class = NULL, scale = TRUE, main = deparse(substitute(x)), 
-          ...) 
-          
-{
-  if (!inherits(x, "randomForest")) 
-    stop("This function only works for objects of class `randomForest'")
-  imp <- importance(x, class = class, scale = scale, type = type, 
-                    ...)
-  if (ncol(imp) > 2) 
-    imp <- imp[, -(1:(ncol(imp) - 2))]
-  nmeas <- ncol(imp)
-  if (nmeas > 1) {
-    op <- par(mfrow = c(1, 2), mar = c(4, 5, 4, 1), mgp = c(2, 
-                                                            0.8, 0), oma = c(0, 0, 2, 0), no.readonly = TRUE)
-    on.exit(par(op))
-  }
-  for (i in 1:nmeas) {
-    ord <- if (sort) 
-      rev(order(imp[, i], decreasing = TRUE)[1:n.var])
-    else 1:n.var
-    xmin <- if (colnames(imp)[i] %in% c("IncNodePurity", 
-                                        "MeanDecreaseGini")) 
-      0
-    else min(imp[ord, i])
-    dotchart(imp[ord, i], ylab = "", 
-             main = if (nmeas == 1) 
-               main
-             else NULL, xlim = c(xmin, max(imp[, i])), ...)
-  }
-  if (nmeas > 1) 
-    mtext(outer = TRUE, side = 3, text = main, cex = 1.2)
-  invisible(imp)
+  # Grab importance metrics
+  imp <- importance(rf.fit.ft, scale = TRUE)
+
+  # Select last two columns which contain importance metrics
+  imp <- imp[, -(1:(ncol(imp) - 2))]
+
+  # Change names for metrics
+  colnames(imp) <- c("Gini Importance", "Accuracy Importance")
+
+  # Change predictor names
+  row.names(imp) <- c('Rainfall seaonality', 'MAP', 'Mean Tmax', 'Minimum RH',
+                        'Tmax', 'Precipitation', 'Solar radiation (Jan)',
+                        'Soil bulk density', 'Solar radiation (Jul)',
+                        'Optimal LAI', 'Plan curvature', 'Profile curvature',
+                        'Wetness index', '% clay')[1:nrow(imp)]
+
+  # Plot the importance of each variable; sort values
+  varImpPlot(rf.fit.ft, main = paste(colnames(imp)[1], "vs", colnames(imp)[2]),
+             labels = rev(row.names(imp)), sort = TRUE)
 }
 
-get.import.order.func <- function(fit.nm){
-  x <- as.data.frame(fit.nm$importance)
-  x <- x[order(x[,ncol(x)],decreasing = T),]
-  x.2 <- x[order(x[,ncol(x)-1],decreasing = T),]
-  return(data.frame(gini = row.names(x),
-                    acu = row.names(x.2)))
-}
-
-plot.importance.func <- function(rf.fit.ft,
-                                 l.out.1,
-                                 l.out.2){
-  # readdata
-  x <- get.import.order.func(rf.fit.ft)
-  
-  # change names
-  x[x == 'pr.seaonality'] <- 'Rainfall seaonality'
-  x[x == 'map'] <- 'MAP'
-  x[x == 'tmax.mean'] <- 'Mean Tmax'
-  x[x == 'rh.min'] <- 'Minimum RH'
-  x[x == 'tmax'] <- 'Tmax'
-  x[x == 'rain'] <- 'Precipitation'
-  x[x == 'rad.short.jan'] <- 'Solar radiation (Jan)'
-  x[x == 'soil.density'] <- 'Soil bulk density'
-  x[x == 'rad.short.jul'] <- 'Solar radiation (Jul)'
-  x[x == 'lai.opt.mean'] <- 'Optimal LAI'
-  x[x == 'curvature_plan'] <- 'Plan curvature'
-  x[x == 'curvature_profile'] <- 'Profile curvature'
-  x[x == 'wi'] <- 'Wetness index'
-  x[x == 'clay'] <- '% clay'
-  # plot
-  varImpPlot(rf.fit.ft,main = '',pch=16,labels  = rev(x[,1]),type=1,
-             xlab='Mean decrease in accuracy')
-  legend('topleft',legend = l.out.1,bty='n')
-  varImpPlot(rf.fit.ft,main = '',pch=16,labels  = rev(x[,2]),type=2,
-             xlab='Mean decrease in Gini Index')
-  legend('topleft',legend = l.out.2,bty='n')
-}
-
-pdf('figures/importance.pdf',width = 5*2,height = 5*3)
-par(mfrow=c(3,2),mar=c(5,5,1,1))
-
-# 1
-plot.importance.func(rf.fit.ft = rf.fit.ft,
-                     l.out.1 = '(a) Fuel type',
-                     l.out.2 = "(b)")
+# Call function to plot importance
+plot.importance.func(rf.fit.ft)
 
 dev.off()
