6cb9cddae6efb8e1bfb40ae38a20a7bfa396a9b6
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-12-16 14:16:42 +1100

    Update plot_future_LAI.R

--- r/plot_future_LAI.R
+++ r/plot_future_LAI.R
@@ -1,117 +1,100 @@
-#####
-read.climate.func <- function(var.in.nm,future_s,exclude.nm = 'noVeg'){
-  rcp45_mid.fn <- list.files(path = 'data/met/future/',
-                             pattern = var.in.nm,recursive = T,full.names = T)
-  
-  rcp45_mid.fn <- rcp45_mid.fn[!rcp45_mid.fn %in% grep(exclude.nm, rcp45_mid.fn)]
-  rcp45_mid.fn <- rcp45_mid.fn[grep(future_s, rcp45_mid.fn)]
-  
-  rcp45.mid.h.c <- sapply(rcp45_mid.fn, function(fn.in){
-    
-    x <- readRDS(fn.in)
-    return(x[[1]])
-  })
-  
-  print(rcp45_mid.fn)
-  raster.new = stack(rcp45.mid.h.c)
+library(raster)
+library(oz)
+
+read.climate.func <- function(var.in.nm, future_s, exclude.nm = 'noVeg') {
+  # Generate list of file names
+  file_names <- list.files(path = '../data/met/future/',
+                           pattern = var.in.nm,
+                           recursive = T,
+                           full.names = T)
+
+  # Exclude file names based on exclude.nm
+  file_names <- file_names[!file_names %in% grep(exclude.nm, file_names)]
+
+  # Filter for file names that match future_s
+  file_names <- file_names[grep(future_s, file_names)]
+
+  # Read RDS data
+  rasters <- lapply(file_names, readRDS)
+
+  # Generate single raster stack
+  raster.new <- stack(rasters)
+
   return(raster.new)
 }
 
-# ####
+# Read in the data for historical
 pr.hist <- read.climate.func(var.in.nm = 'lai_jan_5km.rds',
-                             future_s =  'history',
+                             future_s = 'history',
                              exclude.nm = '')
+
+# Calculate the mean value for each raster in the stack
 mean.ra.hist <- calc(pr.hist, fun = mean)
 
-
-# 
+# Read in the data for the RCP4.5 mid-century scenario
 pr.rcp45.mid <- read.climate.func(var.in.nm = 'lai_jan_5km.rds',
-                                  future_s =  'rcp45_mid',
+                                  future_s = 'rcp45_mid',
                                   exclude.nm = '')
+
+# Calculate the mean value for each raster in the stack
 mean.ra.45.mid <- calc(pr.rcp45.mid, fun = mean)
-# plot(mean.ra.2 - mean.ra.hist)
-# 
+
+# Read in the data for the RCP4.5 end-of-century scenario
 pr.rcp45.long <- read.climate.func(var.in.nm = 'lai_jan_5km.rds',
-                                   future_s =  'rcp45_long',
+                                   future_s = 'rcp45_long',
                                    exclude.nm = '')
-mean.ra.45.long<- calc(pr.rcp45.long, fun = mean)
-# plot(mean.ra.long - mean.ra.hist)
-# 
+
+# Calculate the mean value for each raster in the stack
+mean.ra.45.long <- calc(pr.rcp45.long, fun = mean)
+
+# Read in the data for the RCP8.5 mid-century scenario
 pr.rcp85.mid <- read.climate.func(var.in.nm = 'lai_jan_5km.rds',
-                                  future_s =  'rcp85_mid',
+                                  future_s = 'rcp85_mid',
                                   exclude.nm = '')
-mean.ra.85.mid <- calc(pr.rcp45.mid, fun = mean)
-# 
+
+# Calculate the mean value for each raster in the stack
+mean.ra.85.mid <- calc(pr.rcp85.mid, fun = mean)
+
+# Read in the data for the RCP8.5 end-of-century scenario
 pr.rcp85.long <- read.climate.func(var.in.nm = 'lai_jan_5km.rds',
-                                   future_s =  'rcp85_long',
+                                   future_s = 'rcp85_long',
                                    exclude.nm = '')
-mean.ra.85.long<- calc(pr.rcp85.long, fun = mean)
-# plot((mean.ra.85.long - mean.ra.hist))
-# 
-############
-pdf('figures/future_LAI.pdf',width = 8,height = 7)
 
-# 
-break.vec <- seq(0,4.5,by=0.5)
-library(raster)
-plot(mean.ra.hist,main = ' Current',breaks = break.vec,col=(topo.colors(length(break.vec)-1)))
-library(oz)
-vic(add=T,col='grey',lwd=3)
+# Calculate the mean value for each raster in the stack
+mean.ra.85.long <- calc(pr.rcp85.long, fun = mean)
 
-# 
-break.vec <- seq(-0.6,1.6,by=0.3)
-library(raster)
-plot((mean.ra.45.mid-mean.ra.hist),main = ' RCP4.5 (2045-2060) - Current',
-     breaks = break.vec,col=(topo.colors(length(break.vec)-1)))
-library(oz)
-vic(add=T,col='grey',lwd=3)
-# 
-# break.vec <- seq(20,160,by=20)
-library(raster)
-plot((mean.ra.45.long-mean.ra.hist),main = ' RCP4.5 (2085-2100) - Current',
-     breaks = break.vec,col=(topo.colors(length(break.vec)-1)))
-library(oz)
-vic(add=T,col='grey',lwd=3)
-# 
-# break.vec <- seq(20,160,by=20)
-library(raster)
-plot((mean.ra.85.mid-mean.ra.hist),main = ' RCP8.5 (2045-2060) - Current',
-     breaks = break.vec,col=(topo.colors(length(break.vec)-1)))
-library(oz)
-vic(add=T,col='grey',lwd=3)
-# 
-# break.vec <- seq(20,160,by=20)
-library(raster)
-plot((mean.ra.85.long-mean.ra.hist),main = ' RCP8.5 (2085-2100) - Current',
-     breaks = break.vec,col=(topo.colors(length(break.vec)-1)))
-library(oz)
-vic(add=T,col='grey',lwd=3)
-dev.off()
+# Define a vector of scenarios to plot
+scenarios <- c("RCP4.5 (2045-2060)",
+               "RCP4.5 (2085-2100)",
+               "RCP8.5 (2045-2060)",
+               "RCP8.5 (2085-2100)")
 
-# plot only the end of century#######
-png('figures/future_LAI.png',width = 800,height = 300)
-par(mfrow=c(1,2),mar=c(3,3,1,5))
+# Define a vector of rasters to plot
+rasters <- list(mean.ra.45.mid - mean.ra.hist, mean.ra.45.long - mean.ra.hist,
+                mean.ra.85.mid - mean.ra.hist, mean.ra.85.long - mean.ra.hist)
 
-# a
-mean.ra.hist <- mask(mean.ra.hist, shape.vic)
-break.vec <- seq(0,3,by=0.5)
+# Define a vector of color breaks for historical
+hist.breaks <- seq(0, 4.5, by = 0.5)
 
-plot(mean.ra.hist,
-     breaks = break.vec,
-     col=rev(topo.colors(length(break.vec)-1)),
-     box=FALSE,bty='n')
-myText = bquote((g)~Current~LAI~(m^2*m^-2))
+# Define a vector of color breaks for future scenarios
+fut.breaks <- seq(-0.6, 1.6, by = 0.3)
 
-legend('topleft',legend = myText,bty='n')
+# Open a PDF file
+pdf("future_LAI_new.pdf", width = 8, height = 7)
 
-# b
-break.vec <- seq(0,1.2,by=0.2)
-mean.ra.85.long <- mask(mean.ra.85.long, shape.vic)
+# Plot historical scenario
+plot(mean.ra.hist, main = "Historical", breaks = hist.breaks,
+     col = topo.colors(length(hist.breaks) - 1))
 
-plot((mean.ra.85.long-mean.ra.hist),main = '',
-     breaks = break.vec,
-     col=rev(hcl.colors(length(break.vec)-1)),
-     box=FALSE,bty='n')
+vic(add = TRUE, col = "grey", lwd = 3)
 
-legend('topleft',legend = '(h) Change in the future',bty='n')
+# Loop through the scenarios and rasters
+for (i in seq_along(scenarios)) {
+  # Plot the difference between future scenarios and historical
+  plot(rasters[[i]], main = paste(scenarios[i], " - Historical"),
+       breaks = fut.breaks, col = topo.colors(length(fut.breaks) - 1))
+  vic(add = TRUE, col = "grey", lwd = 3)
+}
+
+# Close the PDF file
 dev.off()
