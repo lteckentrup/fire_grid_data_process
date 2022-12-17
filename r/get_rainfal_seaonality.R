9c6e2f5db87b100117052f10d31bb8e3a2482f41
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-12-17 14:49:17 +1100

    Clean up

--- r/get_rainfal_seaonality.R
+++ r/get_rainfal_seaonality.R
@@ -1,118 +1,108 @@
-# calculating rainfall seasonality follwoing Feng et al 2013
+# Calculates rainfall seasonality follwoing Feng et al 2013
 # 10.1038/nclimate1907
 
 library(raster)
+library(ncdf4)
+
 get.rain.seaonality.func <- function(fn.in){
-  # read file
-  # fn.in <- 'data/met/future/ACCESS1-0/history/history_20002015_monthly_pr.rds'
-  
+  # Read in RDS file
   pr.ra.ls <- readRDS(fn.in)
+
   # stack rasters
   pr.stack <- stack(pr.ra.ls)
+
   # get mean
   pr.mean <- calc(pr.stack, fun=mean)
+
   # get probability for each month
   pr.p.stack <- (pr.stack*365.25/12)/(pr.mean *365.25)
+
   # get the entropy
   d.stack <- pr.p.stack * log(pr.p.stack*12)
-  
   d.sum <- calc(d.stack, fun=sum)
+
   # get index
   pr.seasonality.index <- d.sum * pr.mean / maxValue(pr.mean)
-  
+
   # save outputs
   fn.out <- gsub(pattern = 'monthly_pr',replacement = 'pr_seasonality',x = fn.in)
   saveRDS(pr.seasonality.index,fn.out)
 }
-# do it for all rainfall data###
-fn.vec <- list.files(path = 'data/met/future',
+
+
+# Get list of file names with monthly rainfall data
+fn_vec <- list.files(path = '../data/met/future',
                      pattern = 'monthly_pr.rds',
-                     full.names = T,recursive = T)
-get.rain.seaonality.func
+                     full.names = TRUE, recursive = TRUE)
 
-sapply(fn.vec,get.rain.seaonality.func)
-## example plot
-# fn.in <- 'data/met/future/ACCESS1-0/history/history_20002015_pr_seasonality.rds'
-# plot(readRDS(fn.in))
+# Apply the get.rain.seaonality.func function to each file in fn_vec
+sapply(fn_vec, get.rain.seaonality.func)
 
-# get mean among models####
-fn.vec <- list.files(path = 'data/met/future',
+# Get list of file names with seasonal rainfall data
+fn_vec <- list.files(path = '../data/met/future',
                      pattern = 'pr_seasonality.rds',
-                     full.names = T,recursive = T)
+                     full.names = TRUE, recursive = TRUE)
 
-# 
-get.seasonality.mean.func <- function(rcp.in){
-  fn.hist <- fn.vec[grep(pattern = rcp.in,x = fn.vec)]
-  
-  pr.seaonality.ls <- sapply(fn.hist, readRDS)
-  pr.seaonality.stack <- stack(pr.seaonality.ls)
-  
-  pr.seaonality.mean <- mean(pr.seaonality.stack)
-  saveRDS(pr.seaonality.mean,sprintf('cache/pr_seaonality_%s.rds',rcp.in))
+# Define function to calculate mean of seasonal rainfall data for a given RCP scenario
+get_seasonality_mean_func <- function(rcp) {
+  # Get the file names for the given RCP scenario
+  fn_hist <- fn_vec[grep(pattern = rcp, x = fn_vec)]
+
+  # Read in the seasonal rainfall data for each file
+  pr_seasonality_ls <- sapply(fn_hist, readRDS)
+
+  # Stack the data and calculate the mean
+  pr_seasonality_stack <- stack(pr_seasonality_ls)
+  pr_seasonality_mean <- mean(pr_seasonality_stack)
+
+  # Save the mean data to a file
+  saveRDS(pr_seasonality_mean, sprintf('../cache/pr_seasonality_%s.rds', rcp))
 }
 
-# history####
-# fn.hist <- fn.vec[grep(pattern = 'history',x = fn.vec)]
-# 
-# pr.seaonality.ls <- sapply(fn.hist, readRDS)
-# pr.seaonality.stack <- stack(pr.seaonality.ls)
-# 
-# pr.seaonality.mean <- mean(pr.seaonality.stack)
-# saveRDS(pr.seaonality.mean,'cache/pr_seaonality_history.rds')
+get_seasonality_mean_func(rcp = 'history')
+get_seasonality_mean_func(rcp = 'rcp45_mid')
+get_seasonality_mean_func(rcp = 'rcp45_long')
+get_seasonality_mean_func(rcp = 'rcp85_long')
+get_seasonality_mean_func(rcp = 'rcp85_mid')
 
-get.seasonality.mean.func(rcp.in = 'history')
-# rcp####
-get.seasonality.mean.func(rcp.in = 'rcp45_mid')
-get.seasonality.mean.func(rcp.in = 'rcp45_long')
-get.seasonality.mean.func(rcp.in = 'rcp85_long')
-get.seasonality.mean.func(rcp.in = 'rcp85_mid')
+### Seasonality in SILO reanalysis
+met.nm <-  list.files(path = '../data/met/precip_silo/',
+                      pattern = '.nc',full.names = T,recursive = F)
 
+get_rain_ra_func <- function(met_nm, mon) {
+  # Open netCDF file
+  rain_nc <- nc_open(met_nm)
 
-# get silo seaonality####
-# get file
-met.nm <-  list.files(path = 'data/met/precip_silo/',
-                      pattern = '.nc',full.names = T,recursive = F)  
+  # Get the longitude and latitude
+  lon_vec <- ncvar_get(rain_nc, 'lon')
+  lat_vec <- ncvar_get(rain_nc, 'lat')
 
-get.rain.ra.func <- function(met.nm.in,mon.in){
-  rain.nc <- nc_open(met.nm.in)
-  # get gps
-  lon.vec <- ncvar_get(rain.nc, 'lon')
-  lat.vec <- ncvar_get(rain.nc, 'lat')
-  # get rain
-  rain.df <- try(ncvar_get(rain.nc, 'monthly_rain'))
-  
-  rain.mon.df <- rain.df[,,mon.in]
-  rain.raster <- raster(t(rain.mon.df),
-                        xmn=min(lon.vec), xmx=max(lon.vec), ymn=min(lat.vec), ymx=max(lat.vec))
-  met.raster.right <- flip(rain.raster, direction='y')
-  met.raster.right[is.infinite(met.raster.right)] <- NA
+  # Get the rainfall data
+  rain_df <- try(ncvar_get(rain_nc, 'monthly_rain'))
 
-  return(met.mean)
+  # Extract the desired month's data
+  rain_mon_df <- rain_df[,,mon]
+
+  # Convert the data to a raster object
+  rain_raster <- raster(t(rain_mon_df),
+                        xmn = min(lon_vec), xmx = max(lon_vec),
+                        ymn = min(lat_vec), ymx = max(lat_vec))
+
+  # Flip the raster vertically and replace infinite values with NA
+  met_raster_right <- flip(rain_raster, direction = 'y')
+  met_raster_right[is.infinite(met_raster_right)] <- NA
+
+  # Return the mean value of the raster
+  return(mean(met_raster_right, na.rm = TRUE))
 }
 
-# 
-get.mean.func <- function(mon.in){
-  rain.silo.1 <- sapply(met.nm,get.rain.ra.func,mon.in=mon.in)
-  
-  rain.silo.1.mean <- mean(stack(rain.silo.1))
-  
-  return(rain.silo.1.mean)
+get_mean_func <- function(mon) {
+  # Apply get_rain_ra_func function to each element in the met_nm vector
+  rain_silo_1 <- sapply(met_nm, get_rain_ra_func, mon_in = mon)
+
+  # Calculate mean value of the stacked data
+  rain_silo_1_mean <- mean(stack(rain_silo_1))
+
+  # Return the mean value
+  return(rain_silo_1_mean)
 }
-# 
-rain.silo.seasnoality.ls <- sapply(1:12,get.mean.func)
-pr.stack <- stack(rain.silo.seasnoality.ls)
-# get mean
-pr.mean <- calc(pr.stack, fun=sum)
-# get probability for each month
-pr.p.stack <- (pr.stack)/(pr.mean)
-# get the entropy
-d.stack <- pr.p.stack * log(pr.p.stack*12)
-
-d.sum <- calc(d.stack, fun=sum)
-# get index
-pr.seasonality.index <- d.sum * pr.mean / maxValue(pr.mean)
-# plot(pr.seasonality.index)
-
-# ######
-
-saveRDS(pr.seasonality.index,'cache/pr_seaonality_silo.rds')
