a3513899583afe4c477c44af465e13b373c7f093
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-12-17 15:11:51 +1100

    Clean up

--- r/get_silo_annual.R
+++ r/get_silo_annual.R
@@ -1,60 +1,72 @@
-# get annual######
-get.annual.slio.func <- function(fn.path,var.nm,my.fun){
-  # # 
-  # 
-  # fn.path = 'data/met/tmax_silo/',
-  # var.nm = 'max_temp',
-  # my.fun = mean
-  # # 
-  
-  fn.vec <- list.files(fn.path,full.names = T,pattern = '.nc')
-  met.ls <- list()
-  for (fn.i in seq_along(fn.vec)) {
-    
-    fn <- fn.vec[fn.i]
-    
+library(ncdf4)
+library(raster)
+
+# Define function to get annual mean of a meteorological variable from SILO reanalysis
+get.annual.slio.func <- function(fn.path, var.nm, my.fun){
+  # Get list of file names
+  fn.vec <- list.files(fn.path, full.names = TRUE, pattern = '.nc')
+
+  # Use lapply to apply a function over the list of file names
+  met.ls <- lapply(fn.vec, function(fn){
+    # Open NetCDF file
     nc.tmp <- nc_open(fn)
-    names(nc.tmp$var)
-    tmax <- ncvar_get(nc.tmp,var.nm)
-    # 
-    lon.vec <-nc.tmp$dim$lon$vals
-    lat.vec <-nc.tmp$dim$lat$vals
-    # 
-    select.lon <- which(lon.vec>132 & lon.vec < 150)
-    select.lat <- which(lat.vec> -40&lat.vec< -29)
-    # 
-    tmax.vic <- tmax[select.lon,select.lat,]
-    tmp <- apply(tmax.vic, MARGIN = c(1,2), max, na.rm = TRUE)
-    # 
+
+    # Extract meteorological variable from NetCDF file
+    tmax <- ncvar_get(nc.tmp, var.nm)
+
+    # Longitude and latitude
+    lon.vec <- nc.tmp$dim$lon$vals
+    lat.vec <- nc.tmp$dim$lat$vals
+
+    # Longitude and latitude indices for Victoria
+    select.lon <- which(lon.vec > 132 & lon.vec < 150)
+    select.lat <- which(lat.vec > -40 & lat.vec < -29)
+
+    # Select data for Victoria
+    tmax.vic <- tmax[select.lon, select.lat, ]
+
+    # Get maximum value for each grid cell in Victoria
+    tmp <- apply(tmax.vic, MARGIN = c(1, 2), max, na.rm = TRUE)
+
+    # Convert maximum values to raster object
     tmp.ra <- flip(t(raster((tmp))))
-    extent(tmp.ra) <- c(xmn=132, xmx=150, ymn=-40, ymx=-29)
-    
-    met.ls[[fn.i]] <- tmp.ra
-  }
-  
+
+    # Set extent of raster object to Victoria
+    extent(tmp.ra) <- c(xmn = 132, xmx = 150, ymn = -40, ymx = -29)
+
+    # Return raster object
+    tmp.ra
+  })
+
+  # Stack raster objects into a single object
   met.ra.stack <- stack(met.ls)
+
+  # Calculate mean of stacked raster object
   met.ra.mean <- mean(met.ra.stack)
- return(met.ra.mean)
+
+  # Return mean raster object
+  return(met.ra.mean)
 }
-tmax.ra <- get.annual.slio.func(fn.path = 'data/met/tmax_silo/',
+
+# Maximum temperature
+tmax.ra <- get.annual.slio.func(fn.path = '../data/met/tmax_silo/',
                                 var.nm = 'max_temp',
                                 my.fun = mean)
 
-
-# rh.ra <- get.annual.slio.func(fn.path = 'data/met/rh_silo/',
-#                                 var.nm = 'rh_tmax',
-#                                 my.fun = mean)
-
-prcp.ra <- get.annual.slio.func(fn.path = 'data/met/precip_silo/',
-                              var.nm = 'monthly_rain',
-                              my.fun = mean)
-
-vpd.ra <- get.annual.slio.func(fn.path = 'data/met/vpd_silo/',
-                                var.nm = 'vp_deficit',
+# Precipitation
+prcp.ra <- get.annual.slio.func(fn.path = '../data/met/precip_silo/',
+                                var.nm = 'monthly_rain',
                                 my.fun = mean)
-plot(vpd.ra/10)
+
+# Vapor pressure deficit
+vpd.ra <- get.annual.slio.func(fn.path = '../data/met/vpd_silo/',
+                               var.nm = 'vp_deficit',
+                               my.fun = mean)
+
+# Save stacked raster object with annual mean values of maximum temperature,
+# precipitation, and vapor pressure deficit to RDS
+
 saveRDS(stack(list(tmax = tmax.ra,
-                   # rh = rh.ra,
-                   prcp=prcp.ra*12,#month mean to annual
-                   vpd = vpd.ra/10)),#hPa to kPa
-        'data/met/slio.met.annual.rds')
+                   prcp = prcp.ra * 12, # month mean to annual
+                   vpd = vpd.ra / 10)), # hPa to kPa
+        '../cache/slio.met.annual.rds')
